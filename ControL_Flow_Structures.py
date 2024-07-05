7afc5a6bc48d04298b0ab25b5c04404b047750fc
Ntseno Ntseno <ntsenontseno@hotmail.com>
2024-07-05 21:58:27 +0200

    New file Added

--- ControL_Flow_Structures.py
+++ ControL_Flow_Structures.py
@@ -1,14 +1,14 @@
 userInPut1 = float(input("Enter the first Number: ").strip())
-userInput2 = float(input("Enter the second value: ").strip())
+userInput2 = float(input("Enter the second Number: ").strip())
 
-comparison = f"{userInPut1} is eqaul to {userInput2}"
+def compareFunction(num1, num2):
+    if num1 > num2: 
+        comparison = f"{num1} is greater than {num2}"
+    elif num1 < num2:
+        comparison = f"{num1} is less than {num2}"
+    else:
+        comparison = f"{num1} is eqaul to {num2}"
+    return comparison
 
-if userInPut1 > userInPut1: 
-    comparison = f"{userInPut1} is greater than {userInput2}"
-elif userInPut1 < userInput2:
-    comparison = f"{userInPut1} is less than {userInput2}"
-else:
-    comparison = f"{userInPut1} is eqaul to {userInput2}"
-
-print(comparison)
+print(f"\n{compareFunction(userInPut1, userInput2)}\n")
 
