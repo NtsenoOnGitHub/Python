e35c757d99a34613e7ca212981d34286af05ded1
Ntseno Ntseno <ntsenontseno@hotmail.com>
2024-07-05 22:35:03 +0200

    Added Exercise 2 on the ControL_Flow_Structures

--- ControL_Flow_Structures.py
+++ ControL_Flow_Structures.py
@@ -12,3 +12,20 @@ def compareFunction(num1, num2):
 
 print(f"\n{compareFunction(userInPut1, userInput2)}\n")
 
+# Exercise 2
+# Write an application that obtains
+#two numbers from the user, and displays them, but rejects any input where
+#both numbers are greater than 10 and asks for two new numbers.
+
+
+numOne = int(input("Enter a number: ").strip())
+numTwo = int(input("Enter a number: ").strip())
+
+def logicFunction(number1, number2):
+    while number1 and number2 > 10:
+        number1 = int(input("Enter a number: ").strip())
+        number2 = int(input("Enter a number: ").strip())
+    print(f"Number one is {number1} and number two is {number2}")
+
+logicFunction(numOne, numTwo)
+
