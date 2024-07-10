4f4d45dcea5e54d9753449286471a38907a3e685
NtsenoOnGitHub <52259757+NtsenoOnGitHub@users.noreply.github.com>
2024-07-10 05:37:33 +0200

    Update If_statements_Practice.py

--- If_statements_Practice.py
+++ If_statements_Practice.py
@@ -1,6 +1,6 @@
 #The included code stub will read an integer, , from STDIN.
 # Without using any string methods, try to print the following:
-# 123...n
+# 123...n Where n represent an input from the user
 
 n = int(input("Enter a number: "))
     
