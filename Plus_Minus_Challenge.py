e43be1bcb44490294d8d2e1a4d95884a14e610ad
Ntseno Ntseno <ntsenontseno@hotmail.com>
2024-07-12 06:17:01 +0200

    Added a link to the challenge

--- Plus_Minus_Challenge.py
+++ Plus_Minus_Challenge.py
@@ -1,3 +1,7 @@
+#https://www.hackerrank.com/challenges/one-week-preparation-kit-plus-minus/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=one-week-preparation-kit&playlist_slugs%5B%5D=one-week-day-one
+
+#code can be more efficient
+
 import random
 
 arreSize = int(input("Enter the size of the array: ").strip())
