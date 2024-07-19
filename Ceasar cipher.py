b9350253bd0b48d1bd3167152082f52063be0192
NtsenoOnGitHub <52259757+NtsenoOnGitHub@users.noreply.github.com>
2024-07-19 10:09:39 +0200

    removed the comments

--- Ceasar cipher.py
+++ Ceasar cipher.py
@@ -3,8 +3,6 @@ keyTodecrypt = int(input("Enter a key to decrypt the messgae: "))
 
 alphabets = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 
-# attack
-
 def _encrypter(text, key):
     textToReturn = ""
     counter = 0
